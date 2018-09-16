1883c72f2af7b98060562a18c3acb7b62de2b544
arhex <arhex1@gmail.com>
2018-09-17 02:08:31 +0300

    proba

--- includes/enqueue-script-style.php
+++ includes/enqueue-script-style.php
@@ -7,8 +7,8 @@ function estore_style() {
     wp_enqueue_style( 'estore-style', get_stylesheet_uri(), array('bootstrap-style') );
     wp_enqueue_style('bootstrap-style', get_template_directory_uri().'/assets/css/bootstrap.min.css', array(),null, 'all');
     wp_enqueue_style('font-awesome', get_template_directory_uri().'/assets/css/font-awesome.css');
-
 }
+
 add_action( 'wp_enqueue_scripts', 'estore_scripts' );
 function estore_scripts() {
 
